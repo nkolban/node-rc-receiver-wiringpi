088b8ce1751c7cfbdb9e5684711c4a2a780283d4
kolban <kolban1@kolban.com>
2016-04-30 11:33:34 -0500

    Sync

--- README.md
+++ README.md
@@ -10,16 +10,16 @@ $ npm install rc-receiver-wiringpi
 
 ## Usage
 
-A radio contronl (RC) transmitter sends an encoded signal to an RC receiver.  The RC receiver
+A radio control (RC) transmitter sends an encoded signal to an RC receiver.  The RC receiver
 then decodes the signal and produces a pulse width modulated (PWM) representation of the 
 original inputs.  To think about this, consider a joystick like a slider.  When the slider is
 at one end, it registers 0, when it is at the other end, it registers 100.  If the slider
 is in between the end points, it registers a value proportional to its position.
 
-An RC transmitter common has two joysticks with each joystick able to provide two independent
+An RC transmitter commonly has two joysticks with each joystick able to provide two independent
 values.  Those values are the amount the joystick is north/south and the amount the joystick is
 east/west.  Thus for a transmitter that has two joysticks, there will be a total of four values
-at anytime.  Those values being the amount of north/south on joystick 1, the amount of east/west
+at any one time.  Those values being the amount of north/south on joystick 1, the amount of east/west
 on joystick 1, the amount of north/south on joystick 2 and the amount of east/west on joystick 2.
 Each of these data values is transmitted separately by the transmitter in what is called a
 "channel".  The number of channels provided by a transmitter is usually four or more.  The channels
@@ -32,7 +32,7 @@ a four channel transmitter may be used with a three channel receiver but, obviou
 one of the channels of information.
 
 When using this package, we connect the signal lines out from the RC receiver into digital
-input pins on the Pi.  This software will then examine those pins when requested and tell us the
+input pins on the Pi.  This module will then examine those pins when requested and tell us the
 values currently received from the transmitter.
 
 To use the package, we call:
--- src/rc-receiver-wiringpi.js
+++ src/rc-receiver-wiringpi.js
@@ -4,20 +4,42 @@
 var wpi = require('wiring-pi');
 
 // The set of channels that we are watching.  Each channel contains a record of the form:
-// {
-//   channel: <channel number>,
-//   pin:     <pin for the channel>
-// }
+//
+//  {
+//    channel: <channelNumber>,
+//    pin:     <GPIO pin number>,
+//    invert:  <true/false>,             // Optional
+//    map:     [<lowValue>, <highValue>] // Optional
+//  }
 //
 var channels = [];
 
-function map( x,  in_min,  in_max, out_min, out_max){
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+/**
+ * PRIVATE
+ * Map
+ * @param inValue - The input value
+ * @param in_min - The minimum input value
+ * @param in_max - The maximum input value
+ * @param out_min - The minimum output value
+ * @param out_max - The maximum output value
+ * @returns The input value mapped to the output value
+ */
+function map( inValue,  in_min,  in_max, out_min, out_max){
+  return (inValue - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-// init()
-// Initialize the library.
-//
+/**
+ * PUBLIC
+ * init
+ * Define the set of channels we are going to be watching.  The input is an array
+ * of channel descriptors of the form:
+ * {
+ *   channel: <channelNumber>,
+ *   pin:     <GPIO pin number>,
+ *   invert:  <true/false>,             // Optional
+ *   map:     [<lowValue>, <highValue>] // Optional
+ * }
+ */
 module.exports.init = function(iChannels) {
   wpi.setup('gpio');
   channels = iChannels;
@@ -28,21 +50,32 @@ module.exports.init = function(iChannels) {
 }; // End of init()
 
 
-// getValues()
-// Retrieve the current values of the channels.
-//
+/**
+ * PUBLIC
+ * getValues
+ * Return an array of results from decoding the channels.  Each result is of the format:
+ * {
+ *   channel: <channel number>
+ *   value: <value>
+ * }
+ */
 module.exports.getValues = function() {
-  var results = [];
-  channels.forEach(function(currentChannel){
+  var results = []; // The array of results to be returned.
+  channels.forEach(function(currentChannel) {
     var value = wpi.pulseIn(currentChannel.pin, wpi.HIGH);
     // The value of a PWM pulse is between 1000 (low) and 2000 (high)
     value = Math.round((value-1000)/1000 * 100);
     if (currentChannel.map) {
-      value = map(value, currentChannel.map[0], currentChannel.map[1], 0, 100);
+      value = map(value, 0, 100, currentChannel.map[0], currentChannel.map[1]);
     }
     if (currentChannel.invert) {
-      value = 100 - value;
+      if (currentChannel.map) {
+        value = currentChannel.map[1] - value;
+      } else {
+        value = 100 - value;
+      }
     }
+    // Add the current channel details to the return array.
     results.push({channel: currentChannel.channel, value: value});    
   });
   return results;
